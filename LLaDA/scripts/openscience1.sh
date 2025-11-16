9df728d882ca31ce7a9d3fd5d14e2725bfc4e40f
Mengyu Zhou <mengyu.chou@gmail.com>
2025-11-17 03:29:45 +0800

    Adjust to 8 GPUs.

--- LLaDA/scripts/openscience1.sh
+++ LLaDA/scripts/openscience1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # ========== 环境变量 ==========
-export CUDA_VISIBLE_DEVICES=2,3
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export TORCH_NCCL_TRACE_BUFFER_SIZE=16777216
 export NCCL_ASYNC_ERROR_HANDLING=1
 export NCCL_BLOCKING_WAIT=1
@@ -19,4 +19,4 @@ accelerate launch \
   --train_mode mirror_plus \
   --IS_on_t \
   --batch_size_per_gpu 1 \
-  --grad_accum 16
+  --grad_accum 4

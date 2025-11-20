4e99a5475cd875233a557bf4dbf0e1f836a8c127
Mengyu Zhou <mengyu.chou@gmail.com>
2025-11-20 19:32:29 +0000

    Add evaluation script.

---
+++ LLaDA/evaluate_gsm8k.sh
@@ -0,0 +1,3 @@
+python3 evaluate_gsm8k.py --checkpoint_path "/root/workspace/checkpoints/seed42_llada_gsm8k_ppots_mirror_plus/checkpoint-epoch5"
+python3 evaluate_gsm8k.py --checkpoint_path "/root/workspace/checkpoints/seed731_llada_gsm8k_ppots_mirror_plus/checkpoint-epoch5"
+python3 evaluate_gsm8k.py --checkpoint_path "/root/workspace/checkpoints/seed20231_llada_gsm8k_ppots_mirror_plus/checkpoint-epoch5"
